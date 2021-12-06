7f72f4b5d2f7978ca80f39be7fbf111364c2a1ab
pauloricardolongi <pauloricardolongi@gmail.com>
2021-12-06 00:54:15 -0300

    Programa

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,9 +1,28 @@
 package aplicacao;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entidades.Funcionario;
+
 public class Programa {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		List<Funcionario>list = new ArrayList<>();
+		System.out.println("Entre com o número de funcionários: ");
+		int n = sc.nextInt();
+		
+		for (int i=1; i<=n; i++) {
+			System.out.println("Dados do Funcionário #:" + i);
+			
+		}
+		sc.close();
 
 	}
 
